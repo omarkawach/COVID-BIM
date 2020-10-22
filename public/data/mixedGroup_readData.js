bb806fd36125468e35bb015815a2f2bebbf49b28
Omar Kawach <49499098+omarkawach@users.noreply.github.com>
2020-10-22 17:40:59 -0400

    Add files via upload

--- public/data/mixedGroup_readData.js
+++ public/data/mixedGroup_readData.js
@@ -1,9 +1,12 @@
-d3.csv("/data/sandpiles.csv", function(data) {
+d3.csv("/data/state_change_small.csv", function(data) {
 	window.data = [];
 	window.legendOFF = true;
 	
 	window.max_x = d3.max(data, d => +d.x) + 1;
 	window.max_y = d3.max(data, d => +d.y) + 1
+	window.max_inhaled = d3.max(data, d => +d.curr_inhaled)
+	window.max_particles = d3.max(data, d => +d.current_state)
+	
 	
 	// window.changedata = [];
 	// var coordinate = [];
@@ -20,8 +23,18 @@ d3.csv("/data/sandpiles.csv", function(data) {
 			window.data[idx] = [];
         }
         
-		window.data[idx].push({ x:+data[i].x, y:+data[i].y, state:+data[i].current_state, type:+data[i].type });
+		window.data[idx].push({ 
+			x:+data[i].x, y:+data[i].y, 
+			previous_inhaled:+data[i].prev_inhaled, 
+			inhaled:+data[i].curr_inhaled, 
+			previous_state:+data[i].previous_state, 
+			state:+data[i].current_state, 
+			prev_type:+data[i].prev_type,
+			type:+data[i].curr_type });
+			//type:+data[i].type });
 
 	}
+	// First time step occurs twice, so just remove it.
+	window.data.shift()
 
 });
